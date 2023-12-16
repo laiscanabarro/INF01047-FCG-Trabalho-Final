55b9eda81c04909738c730a23641b8679df2e66b
Leonardo Dutra <leo.togepi@gmail.com>
2023-12-16 13:11:33 -0300

    Worked, but only by compiling then running the executable.

--- main.cpp
+++ main.cpp
@@ -523,14 +523,14 @@ void LoadShadersFromFiles()
     //    |     |
     //    |     o-- main.exe
     //    |
-    //    +--+ src/
+    //    +--+ shaders/
     //       |
     //       o-- shader_vertex.glsl
     //       |
     //       o-- shader_fragment.glsl
 
-    GLuint vertex_shader_id = LoadShader_Vertex("/src/shader_vertex.glsl");
-    GLuint fragment_shader_id = LoadShader_Fragment("/src/shader_fragment.glsl");
+    GLuint vertex_shader_id = LoadShader_Vertex("../../shaders/shader_vertex.glsl");
+    GLuint fragment_shader_id = LoadShader_Fragment("../../shaders/shader_fragment.glsl");
 
     // Deletamos o programa de GPU anterior, caso ele exista.
     if ( g_GpuProgramID != 0 )
